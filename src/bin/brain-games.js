c2be7de3b456ecdb02463b88c66a7965d94406c2
Aleksandr Bakimatov <aleks.bakimatov@gmail.com>
2018-03-05 23:29:05 +0300

    one more test

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const actual = readlineSync.question('May I have your name? ', { defaultInput: 'NoName:(' });
-
-const sayHi = name => `Hello, ${name}!`;
-
-const greeting = (hi, answer) => {
-  console.log(hi(answer));
-  return process.exit();
-};
+import { sayHi, actual, greeting } from '..';
 
 const acquaintance = () => {
   console.log('Welcome to the Brain Games!');
