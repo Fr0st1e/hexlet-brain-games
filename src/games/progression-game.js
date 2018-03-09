f665438dbb33eeee8c33065c50d9c1d61c0f3de2
Aleksandr Bakimatov <aleks.bakimatov@gmail.com>
2018-03-10 02:22:50 +0300

    added brain-progression game

---
+++ src/games/progression-game.js
@@ -0,0 +1,32 @@
+import { buildGame, getRandomNum } from '..';
+
+const rules = 'What number is missing in this progression?';
+
+const generateData = () => {
+  const progressionLength = getRandomNum(4, 10);
+  const progressionStep = getRandomNum(1, 6);
+  const getProgression = (lengthProgression, stepProgression) => {
+    const startNum = getRandomNum(1, 10);
+    const iter = (length, step, acc) => {
+      if (acc.length === length) {
+        return acc;
+      }
+      return iter(length, step, [Math.max(...acc) + step, ...acc]);
+    };
+    return iter(lengthProgression, stepProgression, [startNum]);
+  };
+  const finalProgression = getProgression(progressionLength, progressionStep);
+  const randomIndex = getRandomNum(0, finalProgression.length);
+  const hideRandomSimbol = (index, arr) => {
+    console.log(arr);
+    const plug = '..';
+    return arr
+      .reduce((acc, item) => (arr.indexOf(item) === index ? [plug, ...acc] : [item, ...acc]), [])
+      .join(' ');
+  };
+  return [hideRandomSimbol(randomIndex, finalProgression), String(finalProgression[randomIndex])];
+};
+
+const play = () => buildGame(rules, generateData);
+
+export default play;
